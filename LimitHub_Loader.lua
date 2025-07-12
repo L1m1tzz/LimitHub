cc6990e11d7b4e9f05939e42e9c9ece149555c54
L1m1tzz <adiesy1031@gmail.com>
2025-07-12 21:38:03 +0800

    Update LimitHub_Loader.lua

--- LimitHub_Loader.lua
+++ LimitHub_Loader.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Loader.lua')))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/0qtCM1WZcy/0", true))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/isDpMIR6LU/0", true))()
