e5077a797e16b459f3d580d8489b846abd2a7dd2
L1m1tzz <adiesy1031@gmail.com>
2025-07-24 17:45:28 +0800

    Update LimitHub_Loader.lua

--- LimitHub_Loader.lua
+++ LimitHub_Loader.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/9ed5W0Jq2jgheiOK/raw", true))()
-loadstring(game:HttpGet("https://pastefy.app/KxwGX6Wc/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/T9tHKhQp/raw"))()
